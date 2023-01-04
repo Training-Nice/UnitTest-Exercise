bb116d86334939e87924b1a741e0ab9f7abf5049
Mauricio Balderrama Ali <74360477+mabatule@users.noreply.github.com>
2023-01-04 18:04:54 -0400

    adding new controller for time zone

---
+++ UnitTest Exercise/Controllers/TimeZoneController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTest_Exercise.Controllers
+{
+    public class TimeZoneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
