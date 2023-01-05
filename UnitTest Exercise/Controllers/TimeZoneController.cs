1542ee63b6439eccb8e23a2df066a18c32d67afa
Mauricio Balderrama Ali <74360477+mabatule@users.noreply.github.com>
2023-01-04 23:18:05 -0400

    Add to controller with diferents APIs

--- UnitTest Exercise/Controllers/TimeZoneController.cs
+++ UnitTest Exercise/Controllers/TimeZoneController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using NodaTime;
 using TimeZoneConverter;
 using UnitTest_Exercise.BusinessLogicLayer;
+using UnitTest_Exercise.DataAccessLayer.Repository;
+using UnitTest_Exercise.DataAccessLayer.Repository.Interrfaces;
 
 namespace UnitTest_Exercise.Controllers
 {
@@ -9,36 +12,116 @@ namespace UnitTest_Exercise.Controllers
     [ApiController]
     public class TimeZoneController : Controller
     {
+        private readonly ITimeRepository _timeRepository;
+        public IConfiguration _configuration { get; set; }
+
+        public TimeZoneController(ITimeRepository timeRepository, IConfiguration configuration)
+        {
+            _timeRepository = timeRepository;
+ 
+            _configuration = configuration;
+        }
         [HttpPost]
         [Route("getConvertDate")]
-        public async Task<IActionResult> getConvertTimeZone([FromBody] InputTimeZoneModel inDate)
+        public IActionResult getConvertTimeZone([FromBody] InputTimeZoneModel inDate)
         {
-            TimeZoneInfo universalZone = TimeZoneInfo.Utc;
-            var inputZone = TZConvert.IanaToWindows("America/New_York");
-            DateTime inputTime = new DateTime(2023, 01, 04, 20, 49, 00);
-
-
-
-            TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById(inputZone);
-
-            DateTime aux = TimeZoneInfo.ConvertTimeToUtc(inputTime, easternZone);
-
-
-            var result = new {
-              a= universalZone.DisplayName,
-              b= universalZone.StandardName,
-              inputZone,
-              hourInput= inputTime.TimeOfDay,
-              c=universalZone.DaylightName,
-              aux,
-              hourOutput = aux.TimeOfDay
-
-            };
             try
             {
-
-              
-
+                var result = new { 
+                    ok="ok",
+                    result= _timeRepository.getConvertTimeZone(inDate).TimeOfDay
+                };
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpPost]
+        [Route("getOriginDaylightSavingTime")]
+        public IActionResult getOriginisDaylightSavingTime([FromBody] InputTimeZoneModel inDate)
+        {
+            try
+            {
+                var result = new
+                {
+                    ok = "ok",
+                    result = _timeRepository.isDaylightSavingTime(inDate.Datatime) ? "It's daylight saving time" : "It is not daylight saving time"
+                };
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpPost]
+        [Route("getDestinationDaylightSavingTime")]
+        public IActionResult getDestinationDaylightSavingTime([FromBody] InputTimeZoneModel inDate)
+        {
+            DateTime newDate = _timeRepository.getConvertTimeZone(inDate);
+            try
+            {
+                var result = new
+                {
+                    ok = "ok",
+                    result = _timeRepository.isDaylightSavingTime(newDate) ? "It's daylight saving time" : "It is not daylight saving time"
+                };
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpPost]
+        [Route("getDiferenceTime")]
+        public IActionResult getDiferenceTime([FromBody] InputTimeZoneModel inDate)
+        {
+            try
+            {
+                var result = new
+                {
+                    ok = "ok",
+                    result = _timeRepository.getDiferenceTime(inDate)
+                };
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpPost]
+        [Route("getDiferentFormatDate")]
+        public IActionResult getDiferentFormatDate([FromBody] InputTimeZoneModel inDate)
+        {
+            try
+            {
+                var result = new
+                {
+                    ok = "ok",
+                    result = _timeRepository.getConvertTimeZone(inDate)
+                };
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpPost]
+        [Route("getDiferentFormatTime")]
+        public IActionResult getDiferentFormatTime([FromBody] InputTimeZoneModel inDate)
+        {
+            try
+            {
+                var result = new
+                {
+                    ok = "ok",
+                    result = _timeRepository.getConvertTimeZone(inDate).TimeOfDay
+                };
                 return Ok(result);
             }
             catch (Exception e)
